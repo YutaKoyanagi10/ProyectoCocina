261a575744fff4a8c2491c78a5327c8fd81cc744
YutaKoyanagi10 <yuta.koyanagi@alu.ing.unlp.edu.ar>
2026-01-17 16:18:56 -0300

    added date rows in tables

--- src/main/java/org/proyectococinav2/config/DataBaseConfig.java
+++ src/main/java/org/proyectococinav2/config/DataBaseConfig.java
@@ -50,28 +50,36 @@ public class DataBaseConfig {
             "CREATE TABLE IF NOT EXISTS recipes (" +
                 "id INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "name TEXT NOT NULL UNIQUE," +
-                "instructions TEXT NOT NULL" +
-                ");",
+                "instructions TEXT NOT NULL," +
+                "created_at DATETIME DEFAULT CURRENT_TIMESTAMP, " +
+                "updated_at DATETIME DEFAULT CURRENT_TIMESTAMP" +
+            ");",
             "CREATE TABLE IF NOT EXISTS ingredients (" +
                 "id INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "name TEXT NOT NULL UNIQUE," +
                 "supplier_id INTEGER," +
+                "created_at DATETIME DEFAULT CURRENT_TIMESTAMP, " +
+                "updated_at DATETIME DEFAULT CURRENT_TIMESTAMP," +
                 "FOREIGN KEY(supplier_id) REFERENCES suppliers(id) ON DELETE CASCADE" +
-                ");",
+            ");",
             "CREATE TABLE IF NOT EXISTS recipe_ingredients (" +
                 "recipe_id INTEGER," +
                 "ingredient_id INTEGER," +
                 "serving_per_person REAL NOT NULL CHECK (serving_per_person > 0)," +
                 "unit VARCHAR(10) NOT NULL," +
+                "created_at DATETIME DEFAULT CURRENT_TIMESTAMP, " +
+                "updated_at DATETIME DEFAULT CURRENT_TIMESTAMP," +
                 "PRIMARY KEY (recipe_id, ingredient_id)," +
                 "FOREIGN KEY(recipe_id) REFERENCES recipes(id) ON DELETE CASCADE," +
                 "FOREIGN KEY(ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE" +
-                ");",
+            ");",
             "CREATE TABLE IF NOT EXISTS suppliers (" +
                 "id INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "name TEXT NOT NULL," +
-                "contact_info TEXT NOT NULL" +
-                ");",
+                "contact_info TEXT NOT NULL," +
+                "created_at DATETIME DEFAULT CURRENT_TIMESTAMP, " +
+                "updated_at DATETIME DEFAULT CURRENT_TIMESTAMP" +
+            ");",
         };
 
         try {
@@ -86,12 +94,5 @@ public class DataBaseConfig {
             disconnect();
         }
     }
-
-
-
-
-
-
-
 
 }
